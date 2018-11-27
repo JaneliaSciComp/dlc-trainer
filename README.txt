dbbdf87f8b3a9ad54f76d93d32480b2e3687c703
Adam L. Taylor <taylora@janelia.hhmi.org>
2018-11-27 16:35:04 -0500

    Set more stuff a-right.

--- README.txt
+++ README.txt
@@ -1,16 +1,40 @@
-Cyvasse
+DLC Trainer
+
+This project is based on the DeepLabCut project:
+
+https://github.com/AlexEMG/DeepLabCut
+
+All the code under the dlc folder is the original DeepLabCut code, and
+therefore carries its own copyright and license (GPLv3).
 
 This project implements a cron job that runs on
 login1.int.janelia.org.  It monitors a folder (usually named something
-like /something/something/cyvasse-drop) for video files, and when it
-finds them it runs compresses them using ffmpeg (and the HEVC codec),
-placing the output .mp4 file in a parallel location, typically named
-something like
+like /something/something/dlc-trainer-drop) for targets folders, and
+when it finds them it uses DeepLabCut to train a model on the targets.
+It outputs a "model folder" containing the model in a parallel
+location, typically named something like
 
-    /something/something/cyvasse-drop-output .
+    /something/something/dlc-trainer-drop-output .
 
-Before installing Cyvasse, you should figure out what user the cronjob
-will run as, and then do the build while logged in as that user.
+Before installing DLC Trainer, you should figure out what user the
+cronjob will run as, and then do the build while logged in as that
+user.
+
+Before building DLC Trainer, you should figure out what user the
+cronjob will run as, and then do the build while logged in as that
+user.
+
+To build, you first need to build the Singularity image within which
+DeepLabCut runs.  To do this, you need a linux box with root
+privleges, and with Singularity installed (as of this writing, I last
+used an Ubuntu 18.04.1 box).  Do this to build the image:
+
+    sudo singularity build dlc.simg dlc.def
+
+(Make sure you delete any old image named dlc.simg, or else
+singularity will complain.)  After this completes, you may want to
+chown/chmod dlc.simg to make it owned by you, and have normal file
+permissions.
 
 Next, create a file named "configuration" that points to the folders
 you want to use for input and output.  See configuration-svoboda for
@@ -21,13 +45,11 @@ keep, do
 
     ./turn-on
 
-to add the cyvasse-launching line to the crontab file.  (You can
+to add the DLC-Trainer-launching line to the crontab file.  (You can
 delete this line with the ./turn-off bash script.)
 
 After this, the contents of the input folder should automatically get
 processed every 5 minutes.
 
 ALT
-2018-11-24
-
-
+2018-11-27
--- train_model_and_delete_input_folder.py
+++ train_model_and_delete_input_folder.py
@@ -2,98 +2,40 @@
 
 import sys
 import os
-import tempfile
-import dlct
-#import shutil
-import pathlib
-from compress_video import compress_video
+import shutil
+#import pathlib
+#import delectable.dlct as dlct
+from delectable.train_model import train_model
 
 
-# def frame_rate_as_rational_string_from_video_file_name(video_file_name) :
-#     # Use ffprobe to get the LCM frame rate of a video.
-#     # These LCM frame rates seem to be better preserved than the average frame rate.
-#     command = ['ffprobe', '-v', '0', '-of', 'csv=p=0', '-select_streams', '0', '-show_entries', 'stream=r_frame_rate', video_file_name]
-#     (return_code, stdout_as_string, stderr_as_string) = dlct.system(command)
-#     if return_code != 0:
-#         raise RuntimeError(
-#             'There was a problem running ffprobe to determine the frame rate of %s, return code %d:\n\nstdout: %s\n\nstderr: %s\n' % (
-#                 video_file_name, return_code, stdout_as_string, stderr_as_string))
-#     return stdout_as_string.strip()  # should be a string that looks like a rational number, e.g. '1/1', '30000/1001'
-
-
-def delete_input_file_and_empty_ancestral_folders(input_file_path_as_string, root_input_folder_path_as_string) :
-    input_file_path = pathlib.Path(input_file_path_as_string)
-    root_input_folder_path = pathlib.Path(root_input_folder_path_as_string)
-
-    # Remove the input file itself
-    try :
-        os.remove(str(input_file_path))
-    except Exception as e :
-        print('Tried to delete input file %s, but was unable to do so because:' % str(input_file_path))
-        print(e)
-        return        
-    
-    # We want to remove that contained the input file, if it is now
-    # empty.  And we want to do this recursively up the containing
-    # super-folders, until we reach the root input folder, which we do
-    # *not* want to delete
-
-    # Check that the input file was actually inside the root input folder before we do this
-    common_path = pathlib.Path(dlct.common_prefix_path(str(root_input_folder_path), str(input_file_path)))
-    if common_path != root_input_folder_path :
-        print('Internal error: When checking for empty input folders, the single network path (%s) is not the common path for it and the input file path (%s)' %
-              (str(root_input_folder_path), 
-               str(input_file_path)))
-        return
-
-    # Delete the parent of the input file if it's now empty, and then
-    # recurse up the containing folders, stopping at the root input
-    # folder.
-    target_folder_path = input_file_path.parent
-    is_done = ( target_folder_path == root_input_folder_path )
-    while not is_done :
-        contents = os.listdir(str(target_folder_path)) 
-        if dlct.is_empty(contents) :
-            try :
-                os.rmdir(str(target_folder_path)) 
-            except Exception as e :
-                print('Tried to delete folder %s, but was unable to do so for some reason' % str(target_folder_path))
-                return    
-            target_folder_path = target_folder_path.parent
-            is_done = ( target_folder_path == root_input_folder_path )
-        else :
-            is_done = True 
-# end of function
-
-
-def compress_video_and_delete_input_file(root_input_video_path, 
-                                         input_video_path,
-                                         lock_file_path,
-                                         output_video_path):
+def train_model_and_delete_input_folder(targets_folder_path,
+                                        lock_file_path,
+                                        model_folder_path):
     try:
-        compress_video(input_video_path, output_video_path)
+        train_model(targets_folder_path, model_folder_path)
     except Exception as e:
         # Remove the lock file
         if os.path.exists(lock_file_path) :
             os.remove(lock_file_path)
         raise e
+
     # Remove the lock file
     if os.path.exists(lock_file_path) :
         os.remove(lock_file_path)
-    delete_input_file_and_empty_ancestral_folders(input_video_path, root_input_video_path)
+
+    # Remove the targets folder
+    if os.path.exists(targets_folder_path) :
+        shutil.rmtree(targets_folder_path)
+
 
 #
 # main
 #
-
 if __name__ == "__main__" :
     # Get the arguments
-    root_input_video_path = os.path.abspath(sys.argv[1])
-    input_video_path = os.path.abspath(sys.argv[2])
-    lock_file_path = os.path.abspath(sys.argv[3])
-    output_video_path = os.path.abspath(sys.argv[4])
-    compress_video_and_delete_input_file(root_input_video_path,
-                                         input_video_path,
-                                         lock_file_path,
-                                         output_video_path)
-
+    targets_folder_path = os.path.abspath(sys.argv[1])
+    lock_file_path = os.path.abspath(sys.argv[2])
+    model_folder_path = os.path.abspath(sys.argv[3])
+    train_model_and_delete_input_folder(targets_folder_path,
+                                        lock_file_path,
+                                        model_folder_path)
