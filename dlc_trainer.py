a0d143da29dca3c73488486772aecba71ac0d699
Adam L. Taylor <taylora@janelia.hhmi.org>
2018-11-27 15:44:34 -0500

    Fixes to dlc_trainer.py

--- dlc_trainer.py
+++ dlc_trainer.py
@@ -5,26 +5,28 @@ import os
 import subprocess
 import time
 import pathlib
-import delectable.dlct as dlct
+import shutil
+#import delectable.dlct as dlct
 
 
-def process_targets_folder(singularity_image_path, leaf_script_path, root_input_folder_path, targets_folder_path, root_output_folder_path, output_folder_path, n_submitted) :
+def process_targets_folder(singularity_image_path, leaf_script_path, targets_folder_path, model_folder_path,
+                           n_submitted) :
     # Go training for each targets folder
     print("Going to do training for targets in %s" % targets_folder_path)
-    if os.path.exists(output_folder_path) :
-        if os.path.isfile(output_folder_path) :
-            print("An file exists at output location %s.  Not submitting a job." 
-                  % output_folder_path)            
+    if os.path.exists(model_folder_path) :
+        if os.path.isfile(model_folder_path) :
+            print("An file exists at output location %s.  Not submitting a job."
+                  % model_folder_path)
             do_it = False
         else :
-            if os.path.isdir(output_folder_path) :
+            if os.path.isdir(model_folder_path) :
                 # Run the script only if the source is more recent than the target
                 # and also if the the source has not been modified in the waiting period.
                 # This last is to prevent files that are currently being written from being analyzed.
                 waiting_period = 300  # seconds
                 current_time = time.time()
                 source_modification_time = os.path.getmtime(targets_folder_path)
-                target_modification_time = os.path.getmtime(output_folder_path) 
+                target_modification_time = os.path.getmtime(model_folder_path)
                 print("current time: %s" % current_time)
                 print("source mod time: %s" % source_modification_time)
                 print("target mod time: %s" % target_modification_time)                      
@@ -32,38 +34,39 @@ def process_targets_folder(singularity_image_path, leaf_script_path, root_input_
                           ( current_time > source_modification_time + waiting_period ) )
             else :
                 # The file exists, but is neither a file or a folder.  WTF?
-                print("An object exists at output location %s, but it's neither a file nor a folder.  Not submitting a job." 
-                      % output_folder_path)
+                print(("An object exists at output location %s, but it's neither a file nor a folder.  " +
+                       "Not submitting a job.")
+                      % model_folder_path)
                 do_it = False
     else :
         # target file does not exist
         do_it = True
 
-    lock_file_path = output_folder_path + ".lock"
+    lock_file_path = model_folder_path + ".lock"
     do_it_for_reals = do_it and not os.path.exists(lock_file_path)
     if do_it_for_reals :
         #subprocess.call(['/usr/bin/touch', lock_file_path])
         pathlib.Path(lock_file_path).touch()
-        if os.path.exists(output_folder_path) :
-            shutil.rmtree(output_folder_path)
-        stdout_file_path = output_folder_path + '-training-stdout.txt'
-        stderr_file_path = output_folder_path + '-training-stderr.txt'
+        if os.path.exists(model_folder_path) :
+            shutil.rmtree(model_folder_path)
+        stdout_file_path = model_folder_path + '-training-stdout.txt'
+        stderr_file_path = model_folder_path + '-training-stderr.txt'
         print("stdout_file_path: %s" % stdout_file_path)
         print("stderr_file_path: %s" % stderr_file_path)
-        print("root_input_folder_path: %s" % root_input_folder_path)
+        #print("root_input_folder_path: %s" % root_input_folder_path)
         print("targets_folder_path: %s" % targets_folder_path)
         print("lock_file_path: %s"   % lock_file_path)
-        print("output_folder_path: %s" % output_folder_path)
+        print("output_folder_path: %s" % model_folder_path)
         print("PATH: %s" % os.environ['PATH'])
         print("PWD: %s" % os.environ['PWD'])
         #bsub -o $2-training-stdout.txt -e $2-training-stderr.txt -q gpu_any -n2 -gpu "num=1" singularity exec -B /scratch --nv $PATH_OF_THIS_SCRIPT_FOLDER/dlc.simg python3 $PATH_OF_THIS_SCRIPT_FOLDER/train_model.py $1 $2
         command_list = ['bsub', '-o', stdout_file_path, '-e', stderr_file_path, 
                         '-q', 'gpu_any', 
                         '-n2', 
-                        '-gpu', 'num=1', ...
-                        'singularity', 'exec', '-B', '/scratch', 
+                        '-gpu', 'num=1',
+                        'singularity', 'exec', '-B', '/scratch',
                         '--nv', singularity_image_path, 
-                        'python3', leaf_script_path, targets_folder_path, lock_file_path, output_folder_path]
+                        'python3', leaf_script_path, targets_folder_path, lock_file_path, model_folder_path]
         print('About to subprocess.call(): %s' % repr(command_list))
         return_code = subprocess.call(command_list)
         if return_code == 0 :
@@ -111,9 +114,7 @@ def process_root_folder(singularity_image_path, leaf_script_path, input_folder_p
     for subfolder_name in names_of_subfolders:
         n_submitted = process_targets_folder(singularity_image_path, 
                                              leaf_script_path,
-                                             input_folder_path,
                                              os.path.join(input_folder_path, subfolder_name),
-                                             output_folder_path,
                                              os.path.join(output_folder_path, subfolder_name),
                                              n_submitted)
                     
@@ -134,7 +135,7 @@ if __name__ == "__main__":
     root_input_folder_path = os.path.abspath(sys.argv[1])
     root_output_folder_path = os.path.abspath(sys.argv[2])
     n_submitted = 0
-    n_submitted = process_root_folder(singularity_image_path, ...
+    n_submitted = process_root_folder(singularity_image_path,
                                       leaf_script_path, 
                                       root_input_folder_path, 
                                       root_output_folder_path,
